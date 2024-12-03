909ff3c7e7612470d521639f9c0c6b521ad2cd1a
THALYSON RAFAEL <rafinha.head@gmail.com>
2024-12-03 17:17:00 -0300

    pagina home e erro finalizadas

---
+++ src/pages/Erro/index.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import './erro.css';
+function Erro() {
+  return (
+    <div className="not-found">
+      <h1>404</h1>
+      <h2>Pagina não encontrada</h2>
+      <Link to="/">Veja todos os Tipos de bebida com café!</Link>
+    </div>
+  )
+}
+export default Erro;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import Header from "../../components/Header";
+import Article from "../../components/Article";
+import Footer from "../../components/Footer";
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <Article />
+      <Footer />
+    </div>
+  )
+}
+export default Home;
